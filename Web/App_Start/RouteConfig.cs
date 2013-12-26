63a4c19d1c75e651ba3b43ab3b5a35d5010fddbc
john <john.wood@whiteboard-it>
2013-12-25 18:24:19 -0600

    Added Web project

---
+++ Web/App_Start/RouteConfig.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Web {
+	public class RouteConfig {
+		public static void RegisterRoutes( RouteCollection routes ) {
+			routes.IgnoreRoute( "{resource}.axd/{*pathInfo}" );
+
+
+			/****
+			 * Generic Routing pattern
+			 * {controller} - Page*, Category, Template, Talk, Images
+			 * {name} - Name of the Page to perform the action on
+			 * 
+			 * Notes:
+			 * > Actions in this route are always View.  Use of querystrings 
+			 *		to perform Edit, Move, or Delete.
+			 ***/
+			routes.MapRoute(
+				name: "Generic" ,
+				url: "{ns}:{name}" ,
+				defaults: new {
+					controller = "Article" ,
+					action = "View" ,
+					name = "Home" ,
+					ns = "ns"
+				} ,
+				constraints: new {
+					name = @"^[\w]+$"
+				}
+			);
+
+			routes.MapRoute(
+				name: "Default" ,
+				url: "{name}" ,
+				defaults: new {
+					controller = "Home" ,
+					action = "View" ,
+					name = "name" ,
+					ns = "Main"		// namespace
+				}
+			);
+		}
+	}
+}
---
+++ Web/Models/ArticleModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Wiki.EF;
+
+namespace Wiki.Models {
+	public class ArticleModel : Article {
+
+	}
+}
