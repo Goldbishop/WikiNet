b1aa279d6362bd40532fdec83797b53e0db65f99
john <john.wood@whiteboard-it>
2013-12-25 18:37:39 -0600

    Added Reference to Domain project

--- Repository/WikiContext.cs
+++ Repository/WikiContext.cs
@@ -5,6 +5,7 @@ using System.Data.Objects;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Wiki.Domain;
 
 namespace Wiki.EF {
 	public class WikiContext : ObjectContext, IWikiContext {
