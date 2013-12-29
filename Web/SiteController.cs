df74e5b87967370b62f3e9b971fc719bb8c62458
john <john.wood@whiteboard-it>
2013-12-29 13:09:56 -0600

    Web Project: Added SiteController object.

---
+++ Web/SiteController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Wiki.Domain;
+using Wiki.Repository;
+using Wiki.Service;
+
+namespace Wiki.Web {
+	public class SiteController : Controller {
+		#region Common Properties to all Controllers
+		public IUserRepository UserRepository { get; set; }
+		public IArticleRepository ArticleRepository { get; set; }
+
+		#endregion
+
+		protected override void OnActionExecuting( ActionExecutingContext filterContext ) {
+			
+			base.OnActionExecuting( filterContext );
+		}
+
+		[ChildActionOnly]
+		public ActionResult Menu() {
+
+		}
+	}
+}
