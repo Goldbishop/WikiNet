238f21b81272388d2dbb2e9b41f024b2392a618c
john <john.wood@whiteboard-it>
2013-12-29 13:08:31 -0600

    Web Project: Added SiteInstaller

---
+++ Web/Code/SiteInstaller.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+
+namespace Wiki.Web {
+	public class SiteInstaller : IWindsorInstaller {
+		#region IWindsorInstaller Members
+
+		public void Install( IWindsorContainer container , IConfigurationStore store ) {
+			container.Register(
+				Classes.FromThisAssembly()
+					.BasedOn<IController>()
+						.LifestyleTransient()
+			);
+		}
+
+		#endregion
+	}
+}
