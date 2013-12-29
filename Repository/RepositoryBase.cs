e2063a84fd30f122901f296d3f2f4ab768786aab
john <john.wood@whiteboard-it>
2013-12-29 13:12:42 -0600

    Repository Project: Refactored RepositoryBase to be more in line with implementation patterns.

--- Repository/RepositoryBase.cs
+++ Repository/RepositoryBase.cs
@@ -4,22 +4,6 @@ using Woodsoft.Repository;
 
 namespace Wiki.Repository {
 	public class RepositoryBase<T> : DbRepositoryBase<T> where T : class {
-		public RepositoryBase() : base( new WikiContext( "WikiContext" ) ) { }
-
-		public RepositoryBase( WikiContext context ):base(new DataContext("DataContext")) {
-			this.Context = context;
-		}
-
-		public WikiContext Context { get; private set; }
-	}
-
-	public interface IRepositoryBase<TEntity> where TEntity : class {
-		ObjectSet<TEntity> Entity { get; }
-
-		void Save( TEntity entity );
-		void Delete( TEntity entity );
-
-		IEnumerable<TEntity> AllInformation();
-		IEnumerable<TEntity> FindAll();
+		public RepositoryBase() : base( new WikiContext( "WikiEntities" ) ) { }
 	}
 }
