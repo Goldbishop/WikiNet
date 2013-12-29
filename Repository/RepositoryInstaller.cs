e85ec9e2512287df0b85a07286373138b893579d
john <john.wood@whiteboard-it>
2013-12-29 12:29:46 -0600

    Repository Project: Added RepositoryInstaller

---
+++ Repository/RepositoryInstaller.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+
+namespace Wiki.Repository {
+	public class RepositoryInstaller : IWindsorInstaller {
+		#region IWindsorInstaller Members
+
+		public void Install( IWindsorContainer container , IConfigurationStore store ) {
+			container.Register( Types.FromAssembly( Assembly.GetExecutingAssembly() )
+				.Where( t => t.Namespace.StartsWith( "Wiki.Repository" ) )
+				.WithService.FirstInterface().LifestylePerWebRequest() );
+		}
+
+		#endregion
+	}
+}
