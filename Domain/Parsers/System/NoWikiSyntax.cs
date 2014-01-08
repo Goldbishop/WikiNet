3c928138aac213502163fc67d3b029644f4ebb70
john <john.wood@whiteboard-it>
2014-01-07 18:03:21 -0600

    Domain Project: Added NoWikiSyntax object

---
+++ Domain/Parsers/System/NoWikiSyntax.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Wiki.Domain.Parsers {
+	public class NoWikiSyntax : SystemSyntax , ISyntaxParser {
+		public NoWikiSyntax() : base() {
+			ContentGroup = 1;
+		}
+
+		#region ISyntaxParser Members
+
+		public string Parse( string content ) {
+			//Pattern checks for Beginning tag, <nowiki>, and then
+			//  returns 
+			var patt = new Regex( "<nowiki>(.+?)(</nowiki>)?$" );
+
+			if( patt.IsMatch( content ) ) {
+				var match = patt.Match( content );
+				var val = match.Groups[ 1 ].Value;
+
+				if( match.Groups[ 2 ] != null ) {
+					//TODO: return all content
+				} else {
+					//TODO: Return content to the end of the line
+				}
+
+			}
+
+			return content;
+		}
+
+		#endregion
+	}
+}
