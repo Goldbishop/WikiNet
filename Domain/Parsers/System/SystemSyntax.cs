c92e6949742577077501773dba240c97bd342800
john <john.wood@whiteboard-it>
2014-01-07 18:09:16 -0600

    Domain Project: Added SystemSyntax root object

---
+++ Domain/Parsers/System/SystemSyntax.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Wiki.Domain.Parsers {
+	//Syntax object designed specifically for Internal System parsing
+	public class SystemSyntax : SyntaxParser {
+
+		public bool HasContent { get; set; }
+
+	}
+
+}
